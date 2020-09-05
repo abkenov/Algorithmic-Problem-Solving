76c7a6000fec789bb70d21f87fa77698bd303e8f
Ablan Abkenov <39557007+abkenov@users.noreply.github.com>
2020-09-05 11:01:20 +0600

    Create Tennis_Tournament.cpp

---
+++ Tennis_Tournament.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int ____ = [] () -> int {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    return 0;
+} ( );
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+    long long n, m;
+
+    vector<long long> a;
+    long long size_of_input;
+
+    cin >> n >> m;
+    size_of_input = n;
+
+    a.reserve(size_of_input + 1);
+    a.push_back(0);
+
+    for (int i = 1; i <= size_of_input; ++i)
+    {
+        a[i] = i;
+    }
+
+    for (int i = 1; i <= m; ++i)
+    {
+        long long l, r, winner;
+
+        cin >> l >> r >> winner;
+
+        for (int j = l; j <= r; ++j)
+        {
+            if (a[j] == j && a[j] != winner) {
+                a[j] = winner;
+            } else if (a[winner] == winner && i == m) {
+                a[winner] = 0;
+            }
+        }
+    }
+
+    for (int i = 1; i <= size_of_input; ++i)
+    {
+        cout << a[i] << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
